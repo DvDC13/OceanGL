54337ac283cbf3b2f8c940ed38379d06f3f7a9d7
David Chemaly <davidchemaly1@gmail.com>
2024-01-04 23:11:34 +0100

    add fps

--- application.cpp
+++ application.cpp
@@ -161,6 +161,8 @@ namespace Application
                 ImGui::EndMenu();
             }
 
+            ImGui::Text("   Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
             ImGui::EndMainMenuBar();
         }
 
